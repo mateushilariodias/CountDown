aa76765574f88b2663174882e06d6dbf969685b6
mateushilariodias <mateusilariodias@gmail.com>
2023-08-03 21:15:46 -0300

    Updates to Raphael

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import './App.css'
 import Title from '../components/Title'
 import Counter from '../components/Counter'
-import PaisagemCaminhada from './images/PaisagemCaminhada.jpg'
+import cinemaSalaFranca from './images/cinema_sala_franca.jpg'
 import useCountdown from './hooks/useCountdown'
 
 function App() {
 
-  const myDate = "Sep 15, 2023 17:40:00"; // Substitua por sua própria data
+  const myDate = "Aug 15, 2023 09:15:00"; // Substitua por sua própria data
 
   if (isNaN(new Date(myDate))) {
     console.error("Data inválida fornecida para a contagem regressiva!");
@@ -16,9 +16,9 @@ function App() {
   const [day, hour, minute, second] = useCountdown(myDate);
   
   return (
-    <div className="App" style={{ backgroundImage:`url(${PaisagemCaminhada})`}}>
+    <div className="App" style={{ backgroundImage:`url(${cinemaSalaFranca})`}}>
       <div className='container'>
-        <Title title={"Contagem regressiva para o aniversário do Mateus"} />
+        <Title title={"Contagem regressiva para o aniversário do Rafael"} />
         <div className='countdown-container'>
             <Counter title="Dias" number={day}/>
             <Counter title="Horas" number={hour}/>
